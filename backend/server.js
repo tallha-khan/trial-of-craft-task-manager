25f9a4641d1bf99783ad8044e3389430ebc6a2d1
Talha Mohammed Khan <tallhakhan5432@example.com>
2025-07-31 10:19:11 +0530

    chore: setup backend with express and MongoDB

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+
+app.use(express.json());
+
+
+app.use('/api/auth', require('./routes/auth'));
+
+app.use('/api/projects', require('./routes/projects'));
+
+app.use('/api/tasks', require('./routes/tasks'));
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
